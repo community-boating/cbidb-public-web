4d3e2e6fe0741ecf0c83e99363eb8b4274c0cd01
Jonathan <coleji@bc.edu>
2020-02-16 15:36:16 -0500

    automate redirect route and /jp injection with throw to sentry

--- src/app/routing.tsx
+++ src/app/routing.tsx
@@ -1,3 +1,4 @@
+import * as Sentry from '@sentry/browser';
 import { History } from 'history';
 import * as React from 'react';
 import { Redirect, Route, Router, Switch } from 'react-router';
@@ -6,7 +7,6 @@ import PageWrapper from '../core/PageWrapper';
 import HomePage, { Form as HomePageForm } from '../containers/HomePage';
 import LoginPage from '../containers/LoginPage';
 import Currency from '../util/Currency';
-import extractURLParams from '../util/extractURLParams';
 import asc from './AppStateContainer';
 import { Option, none, some } from 'fp-ts/lib/Option';
 import {getWrapper as getProtoPersonCookie} from "../async/check-proto-person-cookie"
@@ -27,25 +27,34 @@ import { createAcctPageRoute } from './routes/jp/create-acct';
 import { forgotPasswordPageRoute } from './routes/jp/forgot-pw';
 import { forgotPasswordSentPageRoute } from './routes/jp/forgot-pw-sent';
 import { resetPasswordPageRoute } from './routes/jp/reset-pw';
+import PathWrapper from '../core/PathWrapper';
 
-function pathAndParamsExtractor<T extends {[K: string]: string}>(path: string) {
-	return {
-		path,
-		getParams: extractURLParams<T>(path)
+const defaultRouteRender = () => {
+	console.log("uncaught path...", window.location.pathname)
+	const redirectRegex = /^\/redirect\/(.*)$/;
+	const redirectRegexMatchResult = redirectRegex.exec(window.location.pathname);
+	if (redirectRegexMatchResult) {
+		// First see if its a redirect
+		console.log("... its a redirect, going to " + redirectRegexMatchResult[1]);
+		return <Redirect to={'/' + redirectRegexMatchResult[1]} />;
+	} else {
+		console.log("its not a redirect....")
+		const jpRegex = /^\/jp\/(.*)$/;
+		const jpRegexMatchResult = jpRegex.exec(window.location.pathname);
+		if (jpRegexMatchResult) {
+			console.log("it does have a jp in front and its still nothing.  GIve up")
+			return <Redirect to="/" />;
+		} else {
+			console.log("... its doesnt have a jp in front, lets try adding one")
+			Sentry.captureMessage("Uncaught route " + window.location.pathname)
+			return <Redirect to={'/jp/' + PathWrapper.removeLeadingTrailingSlashes(window.location.pathname)} />;
+		}
 	}
-}
-
-export const paths = {
-	resetPassword: pathAndParamsExtractor<{email: string, hash: string}>(""),
-}
+};
 
 // TODO: real shadow components on everything
 export default function (history: History<any>) {
-	// TODO: auto create all these redirect routes?
-	const mustNotBeLoggedIn = [
-		<Route key="/redirect/reserve" path="/redirect/reserve" render={() => <Redirect to="/reserve" />} />,
-		<Route key="/redirect/create-acct" path="/redirect/create-acct" render={() => <Redirect to="/create-acct" />} />,
-		
+	const mustNotBeLoggedIn = [	
 		reservePageRoute.asRoute(history),
 
 		reserveNotesPageRoute.asRoute(history),
@@ -58,7 +67,7 @@ export default function (history: History<any>) {
 
 		resetPasswordPageRoute.asRoute(history),
 
-		<Route key="default" render={() => <PageWrapper
+		<Route key="login" path="/" exact render={() => <PageWrapper
 			key="Loginpage"
 			history={history}
 			component={(urlProps: {}, async: any) => <LoginPage 
@@ -79,22 +88,12 @@ export default function (history: History<any>) {
 				});  // TODO: handle failure
 			}}
 		/>} />,
+
+		<Route key="default" render={defaultRouteRender} />,
 	]
 
 	const mustBeLoggedIn = [
 		<Route key="login" path="/login" render={() => <Redirect to="/" />} />,
-		<Route key="home" path="/redirect/home" render={() => <Redirect to="/" />} />,
-		<Route key="/redirect/checkout" path="/redirect/checkout" render={() => <Redirect to="/checkout" />} />,
-		<Route key={`/redirect${classTimePageRoute.pathWrapper.path}`} path={`/redirect${classTimePageRoute.pathWrapper.path}`} render={() => {
-			const params = pathAndParamsExtractor<{personId: string, typeId: string}>(`/redirect${classTimePageRoute.pathWrapper.path}`).getParams(history.location.pathname);
-			const path = classTimePageRoute.pathWrapper.getPathFromArgs({ personId: params.personId, typeId: params.typeId });
-			return <Redirect to={path} />;
-		}}/>,
-		<Route key={`/redirect${classPageRoute.pathWrapper.path}`} path={`/redirect${classPageRoute.pathWrapper.path}`} render={() => {
-			const params = pathAndParamsExtractor<{personId: string, typeId: string}>(`/redirect${classPageRoute.pathWrapper.path}`).getParams(history.location.pathname);
-			const path = classPageRoute.pathWrapper.getPathFromArgs({ personId: params.personId });
-			return <Redirect to={path} />;
-		}}/>,
 		
 		checkoutPageRoute.asRoute(history),
 
@@ -114,7 +113,7 @@ export default function (history: History<any>) {
 
 		regEmptyPageRoute.asRoute(history),
 
-		<Route key="default" render={() => <PageWrapper
+		<Route key="home" path="/" exact render={() => <PageWrapper
 			key="HomePage"
 			history={history}
 			component={(urlProps: {}, async: HomePageForm) => <HomePage
@@ -131,7 +130,9 @@ export default function (history: History<any>) {
 					return Promise.resolve(welcome);
 				}).catch(err => Promise.resolve(null));  // TODO: handle failure
 			}}
-		/>} />
+		/>} />,
+
+		<Route key="default" render={defaultRouteRender} />,
 	]
 
 	const isLoggedIn = (asc.state.login.authenticatedUserName as Option<string>).isSome();
