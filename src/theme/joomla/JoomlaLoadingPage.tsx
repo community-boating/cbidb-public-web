89c9fa5d441f4b6f22df3d46651b48401cffeff9
Jonathan Cole <coleji@bc.edu>
2020-03-23 19:31:50 -0400

    add spinner img

--- src/theme/joomla/JoomlaLoadingPage.tsx
+++ src/theme/joomla/JoomlaLoadingPage.tsx
@@ -2,15 +2,36 @@ import * as React from "react";
 import Page from "../../components/Page";
 
 interface Props {
+    showSpinner?: boolean
 }
 
 export default class JoomlaLoadingPage extends Page<Props> {
     render() {
-        return (
-            <div className="rt-container">
-                <div className="rt-grid-12">
+        if (this.props.showSpinner) {
+            return (
+                <div className="rt-container">
+                    <div className="rt-grid-12">
+                        <div id="rt-main-column" className="page-content-light">
+                            <div className="rt-block component-block" style={{minHeight: "350px"}}>
+                                <div id="rt-mainbody">
+                                    <div className="component-content rt-joomla">
+                                        <div className="rt-joomla" style={{textAlign: "center"}}>
+                                            <img src="/images/spinner-grey.gif" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        )
+            );
+        } else {
+            return (
+                <div className="rt-container">
+                    <div className="rt-grid-12">
+                    </div>
+                </div>
+            );
+        }
     }
 }
