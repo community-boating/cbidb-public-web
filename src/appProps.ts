1c4b861fea9ce00e5a3532b3f15fe8011e3c786d
Jonathan <coleji@bc.edu>
2020-05-05 11:06:56 -0400

    bungled the merge

--- src/appProps.ts
+++ src/appProps.ts
@@ -1,4 +1,7 @@
 import { AppProps } from './app/AppStateContainer';
+import { PostString } from './core/APIWrapperTypes';
+import {apiw as login} from "./async/authenticate-member"
+import asc from './app/AppStateContainer';
 
 const appProps: AppProps = {
 	isServer: true,
@@ -6,7 +9,15 @@ const appProps: AppProps = {
 	jpDirectorNameLast: "Kotsatos",
 	jpDirectorEmail: "niko@community-boating.org",
 	jpPriceCents: 32500,	// TODO: get from welcome pkg
-	currentSeason: 2020
+	currentSeason: 2020,
+	attemptLoginFunction: (userName: string, payload: PostString) => {
+		return login().send(payload).then(res => {
+			if (res.type == "Success" && res.success) {
+				asc.updateState.login.setLoggedIn(userName);
+				return true;
+			} else return false;
+		})
+	}
 }
 
 export default appProps;
