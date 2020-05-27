491814a80607481812cbe092508e843a56ae37d2
Jonathan <coleji@bc.edu>
2020-05-26 20:33:13 -0400

    create, capture, throw

--- src/util/assertUniqueKeys.ts
+++ src/util/assertUniqueKeys.ts
@@ -1,12 +1,16 @@
+import * as Sentry from "@sentry/browser"
+
 export const assertUniqueKeys: <T extends any>(es: T[]) => T[] = es => {
 	var keys: {[k: string]: true} = {};
 	es.forEach(e => {
 		const key = e.key || "";
 		if (undefined !== keys[key]) {
 			try {
-				throw new Error ("Non unique react key " + key);				
+				const e = new Error ("Non unique react key " + key);				
+				Sentry.captureException(e);
+				throw e;
 			} catch (e) {
-
+				// quietly eat it after throwing to sentry
 			}
 		}
 		keys[key] = true;
