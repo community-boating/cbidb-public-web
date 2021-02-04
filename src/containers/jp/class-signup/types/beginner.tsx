118b2ca12755a8535247c4c3c2f548f1e6f2e779
Jonathan <coleji@bc.edu>
2021-02-04 14:14:19 -0500

    class descriptions

--- src/containers/jp/class-signup/types/beginner.tsx
+++ src/containers/jp/class-signup/types/beginner.tsx
@@ -9,12 +9,16 @@ const data: ClassType = {
 	sessionLength: 2.5,
 	classSize: 12,
 	description: (<React.Fragment>
-		Beginner Sailing includes an orientation to CBI, first sail, kayak safety, shore school, and much more. The first week covers all the basics including rigging, safety rules, and sailing maneuvers; everyone will sail our beginner boats, without an instructor. Students will also learn the basics of kayaking in the first couple days so they can paddle with friends during Junior Program hours. The second week continues with more advanced lessons and games designed to challenge and encourage our students. They’ll be on the water every day; we try very hard to keep them active, in the sun, and having fun. Upon completion of the class, sailors will earn a green rating so they can check out a boat during green flag weather.
-		<br />
-		<br />
-		New juniors with prior sailing experience can begin with more advanced classes; please email Niko Kotsatos, Junior Program Director, at <a href="mailto:niko@community-boating.org">niko@community-boating.org</a> if you would like to request advanced placement.<br />
-		<br />
-		<b>Once you are enrolled in a Beginner Sailing class you will be able to enroll in an Intermediate Sailing class or Paddle Adventure class; sign up first and then check back to this page.</b>
+		Beginner Sailing includes an orientation to CBI, kayak safety, shore school, and much more. The first week covers all the basics including rigging,
+		safety rules, and sailing maneuvers; everyone will sail with a partner in our beginner boats, with an instructor accompanying the group in a powerboat.
+		Students will also learn the basics of kayaking in the first couple days. The second week continues with more advanced lessons and games
+		designed to challenge and encourage our students. They’ll be on the water every day; we try very hard to keep them active, in the sun, and having fun.
+		Upon completion of the class, sailors will earn a green rating which indicates their ability to sail in light wind
+		and qualifies them to take the Intermediate Class next year.
+		<br /><br />
+		New juniors with prior sailing experience can begin with more advanced classes; please email Niko Kotsatos, or Fiona O'Connor, Junior Program Co-Directors,
+		at <a href="mailto:niko@community-boating.org">niko@community-boating.org</a> and <a href="mailto:fiona@community-boating.org">fiona@community-boating.org</a> if you would
+		like to request advanced placement.
 	</React.Fragment>)
 }
 
--- src/containers/jp/class-signup/types/intermediate.tsx
+++ src/containers/jp/class-signup/types/intermediate.tsx
@@ -9,7 +9,17 @@ const data: ClassType = {
 	sessionLength: 2.5,
 	classSize: 12,
 	description: (<React.Fragment>
-		Intermediate Sailing teaches new skills while also polishing the techniques introduced in Beginner Sailing. Students refine their positioning and sail trim, improve their maneuvers and control, and learn to use a jib, all through a combination of lessons and structured play. The first week focuses on sail trim, boat handling, upwind sailing, and skills needed to sail in heavier winds, including reefing. By week 2, we introduce the second Mercury sail, the jib. When a jib is added to the mix, sailing becomes even more fun and sailors really learn to work together as skipper and crew.
+		Intermediate Sailing teaches new skills while also polishing the techniques introduced in Beginner Sailing.
+		Sailing in pairs, students refine their positioning and sail trim, improve their maneuvers and control, and learn to use a jib,
+		all through a combination of lessons and structured play. The curriculum focuses on sail trim, boat handling, upwind sailing,
+		and skills needed to sail in heavier winds, including reefing. If Juniors are ready, we can also introduce the second Mercury sail,
+		the jib. When a jib is added to the mix, sailing becomes even more fun and sailors really learn to work together as skipper and crew.
+		By the end of the week, if a Junior displays competence in the skills above, they may receive their Yellow Rating,
+		which indicates their ability to sail in moderate wind, or even their Red Rating, which indicates their ability to sail in heavy wind with a jib.
+		<br /><br />
+		Juniors who are just moving up to the Intermediate level this year are encouraged to take it twice.
+		If you think your junior is ready to receive their Red Rating by the end of a One-Week Intermediate,
+		keep in mind that the Red Rating would qualify someone to take a one-week advanced class.
 	</React.Fragment>)
 }
 
