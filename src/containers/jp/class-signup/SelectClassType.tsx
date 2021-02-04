da66ab6213cf0aa204924aa21d087ef90cde9db5
Jonathan <coleji@bc.edu>
2021-02-04 15:14:03 -0500

    text: advanced classes title

--- src/containers/jp/class-signup/SelectClassType.tsx
+++ src/containers/jp/class-signup/SelectClassType.tsx
@@ -87,7 +87,7 @@ export default class SelectClassType extends React.Component<Props, State> {
 		const advancedCanSee = advanced.filter(canSeeClass)
 		const advancedRegion = (advancedCanSee.length > 0
 			? (
-				<JoomlaArticleRegion title={<React.Fragment>Next Step: Advanced Classes</React.Fragment>}>
+				<JoomlaArticleRegion title={<React.Fragment>Advanced Classes</React.Fragment>}>
 					{advancedCanSee.map(asDivCurried)}
 				</JoomlaArticleRegion>
 			)
