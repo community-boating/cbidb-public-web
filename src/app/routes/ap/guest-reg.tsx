3183e429407e04efce07e3fb80a67e6bc5faeac5
evan-mccarty <evan.t.mccarty@gmail.com>
2021-03-27 15:03:15 -0400

    path

---
+++ src/app/routes/ap/guest-reg.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import path from "../../paths/ap/guest-reg";
+import PageWrapper from "../../../core/PageWrapper";
+import RouteWrapper from "../../../core/RouteWrapper";
+import { setAPImage } from '../../../util/set-bg-image';
+//import ApPreRegister from '../../../containers/ap/ApPreRegister';
+import FactaLoadingPage from '../../../theme/facta/FactaLoadingPage';
+
