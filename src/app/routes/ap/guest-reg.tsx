de3565bf6ecf78db0c98a0b3a8569242b002aebc
evan-mccarty <evan.t.mccarty@gmail.com>
2021-03-27 15:12:23 -0400

    route

--- src/app/routes/ap/guest-reg.tsx
+++ src/app/routes/ap/guest-reg.tsx
@@ -3,6 +3,15 @@ import path from "../../paths/ap/guest-reg";
 import PageWrapper from "../../../core/PageWrapper";
 import RouteWrapper from "../../../core/RouteWrapper";
 import { setAPImage } from '../../../util/set-bg-image';
-//import ApPreRegister from '../../../containers/ap/ApPreRegister';
+import ApPreRegister from '../../../containers/ap/ApPreRegister';
 import FactaLoadingPage from '../../../theme/facta/FactaLoadingPage';
 
+export const apGuestRegRoute = new RouteWrapper(true, path, history => <PageWrapper
+	key="guestReg"
+	history={history}
+	component={() => <ApPreRegister
+		history={history}
+	/>}
+	urlProps={{}}
+	shadowComponent={<FactaLoadingPage setBGImage={setAPImage} />}
+/>);
