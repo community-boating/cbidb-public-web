de6871b130a232445910cff771bc3d4cae597332
Jonathan <coleji@bc.edu>
2021-04-05 20:01:54 -0400

    add landing page, bg images

---
+++ src/containers/LandingPage.tsx
@@ -0,0 +1,30 @@
+import { History } from 'history';
+import * as React from "react";
+
+import FactaArticleRegion from "@facta/FactaArticleRegion";
+import { setCheckoutImage } from '@util/set-bg-image';
+import FactaMainPage from '@facta/FactaMainPage';
+import { apBasePath } from '@paths/ap/_base';
+import { jpBasePath } from '@paths/jp/_base';
+
+type Props = {
+	history: History<any>
+}
+
+export default class LandingPage extends React.PureComponent<Props> {
+	render() {
+		return <FactaMainPage setBGImage={setCheckoutImage}>
+			<FactaArticleRegion title="Welcome to Community Boating Online!">
+				<div style={{fontSize: "1.1em"}}>
+					To get started, select your program below.
+					<br />
+					<br />
+					<ul>
+						<li><a href={apBasePath.getPathFromArgs({})}>Adult Program</a></li>
+						<li><a href={jpBasePath.getPathFromArgs({})}>Junior Program</a></li>
+					</ul>
+				</div>
+			</FactaArticleRegion>
+		</FactaMainPage>
+	}
+}
