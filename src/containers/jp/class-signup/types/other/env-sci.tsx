e99618221db762242436e8188ce10f558e46972c
Jonathan <coleji@bc.edu>
2021-07-16 08:54:51 -0400

    text: env sci description

--- src/containers/jp/class-signup/types/other/env-sci.tsx
+++ src/containers/jp/class-signup/types/other/env-sci.tsx
@@ -10,7 +10,7 @@ const data: ClassType = {
 	sessionLengthMinutes: 75,
 	classSize: 16,
 	description: (<React.Fragment>
-		Community Boating's Environmental Science program is a STEM program which utilizes sailing as a vessel to explore environmental science, engineering, technology, and math. This program is based off of U.S. Sailing's REACH curriculum, which is a STEM education program created through collaboration between classroom teachers and sailing instructors. Students will have a chance to participate in several experiential learning activities such as building their own anemometer, exploring buoyancy by building clay boats, and learning about weather patterns by creating clouds-in-a-jar.
+		Community Boating's Environmental Science and Physics of Sailing program is a STEM program which utilizes sailing as a vessel to explore physics, environmental science, and how they both govern interactions within ecosystems. This program is in part based off of U.S. Sailing's REACH curriculum, which is a STEM education program created through collaboration between classroom teachers and sailing instructors. Students will have a chance to participate in several experiential learning activities such as building their own anemometer, exploring buoyancy by creating clay boats, and constructing their own water sampling tools. In addition, students will get hands on training and practice with microscopes, slide preparation, water quality testing, and flourometers.
 	</React.Fragment>)
 }
 
