b7c49fffd2dc86a0ab26d144211e841042c62fca
Jonathan Cole <coleji@bc.edu>
2021-08-05 20:17:16 -0400

    content: add adv merc clinic jp class

---
+++ src/containers/jp/class-signup/types/advanced/advanced-merc-clinic.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import { ClassType } from "../../class-description";
+
+
+const data: ClassType = {
+	typeId: 1441,
+	typeName: "Advanced Merc Clinic",
+	prereq: "Merc Yellow Plus or Red",
+	sessionCt: 4,
+	sessionLength: 2.5,
+	classSize: 16,
+	description: (<React.Fragment>
+		A four-day class aimed at yellow plus / red-rated sailors to learn advanced sailing techniques in Mercuries. Topics include rudderless sailing, spinnakers, fine boat handling, and techniques in racing.
+	</React.Fragment>)
+}
+
+export default data; 
