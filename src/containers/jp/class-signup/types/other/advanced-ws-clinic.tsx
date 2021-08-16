9c326b15a4b5a06a9509a6c2a4b2daf753bce014
Jonathan <coleji@bc.edu>
2021-08-16 10:38:46 -0400

    text: update adv jp classes

--- src/containers/jp/class-signup/types/other/advanced-ws-clinic.tsx
+++ src/containers/jp/class-signup/types/other/advanced-ws-clinic.tsx
@@ -6,9 +6,9 @@ const data: ClassType = {
 	typeId: 1264,
 	typeName: "Advanced Windsurfing Clinic",
 	prereq: "Mercury Yellow Rating",
-	sessionCt: 2,
-	sessionLength: 2.5,
-	classSize: 8,
+	sessionCt: 1,
+	sessionLength: 3,
+	classSize: 6,
 	description: (<React.Fragment>
 		Windsurf clinics offer advanced instruction for those who have taken the basic class. Will cover topics based upon the wind and experience level, including bigger sails, smaller boards, upwind sailing, perhaps even harnesses and water starts!
 	</React.Fragment>)
