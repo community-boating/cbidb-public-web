999ab152effe5e02589d381b293878565c244084
Jonathan Cole <jon@jcole.io>
2022-05-03 21:31:06 -0400

    Feat: rental reserve page (#18)
    * add route to same container as guest reg
    
    * add new files
    
    * remove link from login page
    
    * print card on screen
    
    * rename path to /ap/rentals
    
    * render actual barcode vs test
    
    * add forRental boolean prop to api call
    
    * update text

---
+++ src/app/paths/ap/rental-reg.ts
@@ -0,0 +1,3 @@
+import { apBasePath } from "./_base";
+
+export const apRentalPath = apBasePath.appendPathSegment<{}>("/rentals");
--- src/app/routes/ap/rental-reg.tsx
+++ src/app/routes/ap/rental-reg.tsx
@@ -1,17 +1,17 @@
 import * as React from 'react';
-import path	from "@paths/ap/guest-reg";
+import {apRentalPath}	from "@paths/ap/rental-reg";
 import PageWrapper from "@core/PageWrapper";
 import RouteWrapper from "@core/RouteWrapper";
 import { setAPImage } from '@util/set-bg-image';
 import ApGuestRegister, { NONMEM_REG_FLOW } from '@containers/ap/NonmemberRegistration';
 import FactaLoadingPage from '@facta/FactaLoadingPage';
 
-export const apGuestRegRoute = new RouteWrapper(true, path, history => <PageWrapper
-	key="guestReg"
+export const apRentalRegRoute = new RouteWrapper(true, apRentalPath, history => <PageWrapper
+	key="rentalReg"
 	history={history}
 	component={() => <ApGuestRegister
 		history={history}
-		rentalMode={NONMEM_REG_FLOW.GUEST}
+		rentalMode={NONMEM_REG_FLOW.RENTAL}
 	/>}
 	urlProps={{}}
 	shadowComponent={<FactaLoadingPage setBGImage={setAPImage} />}
