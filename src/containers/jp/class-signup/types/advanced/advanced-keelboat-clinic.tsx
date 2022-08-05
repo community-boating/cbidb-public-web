82b5bceb769fea8c9a59c5079eff1830aa74484f
Jonathan Cole <jon@jcole.io>
2022-08-04 20:15:18 -0400

    text: update adv keelboat clinic desc

--- src/containers/jp/class-signup/types/advanced/advanced-keelboat-clinic.tsx
+++ src/containers/jp/class-signup/types/advanced/advanced-keelboat-clinic.tsx
@@ -5,12 +5,12 @@ import { ClassType } from "../../class-description";
 const data: ClassType = {
 	typeId: 1261,
 	typeName: "Advanced Keelboat Clinic",
-	prereq: "Keelboat Green",
-	sessionCt: 4,
+	prereq: "Keelboat Yellow",
+	sessionCt: 3,
 	sessionLength: 2.5,
 	classSize: 6,
 	description: (<React.Fragment>
-		Our keelboats provide a stable platform on which to teach spinnakers, the finer points of sail trim, and to get demonstrations from an instructor in their boat. Advanced topics covered will be based on the week’s wind forecast.
+		An advanced clinic on our keelboats featuring keelboat racing skills and tactics and how to fly a spinnaker!
 	</React.Fragment>)
 }
 
