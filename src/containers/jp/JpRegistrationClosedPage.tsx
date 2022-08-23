1f10262700ddad6e4fefe51328b66f865c1631cc
Jonathan Cole <cole.jonathan.r@gmail.com>
2022-08-23 00:28:37 -0400

    dynamic jp closed year

--- src/containers/jp/JpRegistrationClosedPage.tsx
+++ src/containers/jp/JpRegistrationClosedPage.tsx
@@ -5,12 +5,20 @@ import FactaArticleRegion from 'theme/facta/FactaArticleRegion';
 import { setJPImage } from "util/set-bg-image";
 import { jpBasePath } from "app/paths/jp/_base";
 import FactaMainPage from "theme/facta/FactaMainPage";
+import * as moment from 'moment';
 
 export default class JpRegistrationClosedPage extends React.PureComponent<{history: History<any>}> {
 	render() {
+		const now = moment();
+		const month = Number(now.format("M"));
+		const year = Number(now.format("YYYY"));
+
+		const isPreSeason = month < 7;
+		const closedSeason = isPreSeason ? year-1 : year;
+
 		return <FactaMainPage setBGImage={setJPImage}>
 			<FactaArticleRegion title="Registration is closed.">
-				Registration is closed for the 2021 season. Please keep an eye on our <a target="_blank" href="https://www.community-boating.org">website</a> for when registration will open for 2022.
+				Registration is closed for the {closedSeason} season. Please keep an eye on our <a target="_blank" href="https://www.community-boating.org">website</a> for when registration will open for {closedSeason+1}.
 			</FactaArticleRegion>
 			<FactaButton text="< Back" onClick={() => Promise.resolve(this.props.history.push(jpBasePath.getPathFromArgs({})))}/>
 		</FactaMainPage>
