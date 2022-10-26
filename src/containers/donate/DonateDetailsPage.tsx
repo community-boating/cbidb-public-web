4233e1d4e85b8f417b9985ee49170008fc058de1
Jonathan Cole <jon@jcole.io>
2022-10-26 08:20:18 -0400

    remove debug text

--- src/containers/donate/DonateDetailsPage.tsx
+++ src/containers/donate/DonateDetailsPage.tsx
@@ -378,7 +378,7 @@ export default class DonateDetailsPage extends React.PureComponent<Props, State>
 		return (
 			<FactaMainPage setBGImage={setCheckoutImageForDonations}>
 				{errorPopup}
-				<FactaArticleRegion title={<span>Support Community Boating by making a donation today! {this.props.fundCode.getOrElse("none")}</span>}>
+				<FactaArticleRegion title={<span>Support Community Boating by making a donation today!</span>}>
 					{/* <DonationThirdPartyWidget /> */}
 					<br />
 					Community Boating, Inc. is a private, 501(c)3 non-profit organization operating affordable and accessible programs
