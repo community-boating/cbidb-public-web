718a385e57406f1dce71d7975283bbca7edf0161
Jonathan Cole <jon@jcole.io>
2022-11-28 12:12:15 -0500

    jp reg force open

--- src/app/AppStateContainer.tsx
+++ src/app/AppStateContainer.tsx
@@ -110,7 +110,7 @@ export class AppStateContainer {
 				authenticatedUserName: none
 			},
 			justLoggedIn: false,
-			jpRegistrationClosed: now.isBefore(jpOpen),
+			jpRegistrationClosed: false,
 			jpReservationAllowed: reservationDays[now.format("MM/DD/YYYY")]
 		};
 	}
