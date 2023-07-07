f11a2d476c37235fcdc4a3602414f7585431b45f
Jonathan Cole <jon@jcole.io>
2023-07-07 16:09:34 -0400

    add missing file to prev

---
+++ src/containers/jp/class-signup/types/other/robosub.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import { ClassType } from "../../class-description";
+
+const data: ClassType = {
+	typeId: 1581,
+	typeName: "Robo Sub",
+	prereq: "Mercury Green rating, ages 14-17",
+	sessionCt: 4,
+	sessionLength: 3,
+	classSize: 12,
+	description: (<React.Fragment>
+		RoboSub at CBI connects teamwork, engineering, and technical skills into a unique challenge. Students will build and control remotely operated submersibles to complete various tasks. Experts from the robotics field will share their experience in industry and work alongside the students as they perfect their submersibles. 
+	</React.Fragment>)
+}
+
+export default data; 
