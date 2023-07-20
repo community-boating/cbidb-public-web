060ea0ef5747316baa45ca743928f00251a791c2
Jonathan Cole <jon@jcole.io>
2023-07-20 09:27:28 -0400

    add race committee

---
+++ src/containers/jp/class-signup/types/other/race-committee.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import { ClassType } from "../../class-description";
+
+const data: ClassType = {
+	typeId: 281,
+	typeName: "Learn to be Race Committee",
+	prereq: "Mercury Red (Or Yellow Plus)",
+	sessionCt: 1,
+	sessionLength: 3,
+	classSize: 12,
+	description: (<React.Fragment>
+		Race committee is vital to the functioning of successful sailboat racing, setting and managing the course in accordance with wind direction and conditions,
+		serving as an enforcer and arbiter of the racing rules, and keeping score throughout the regatta.
+		In this one-day, three-hour class, kids will learn how to run skippers meetings, manage start sequences, tally scores, and more!
+		Mercury yellow plus or red rating required. Must be 14 or older.
+	</React.Fragment>)
+}
+
+export default data; 
