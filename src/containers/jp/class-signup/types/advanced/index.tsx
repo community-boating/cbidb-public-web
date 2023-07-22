51b8def07e4ee7becdc9e9187a7a5026c8f1b97e
Jonathan Cole <jon@jcole.io>
2023-07-22 11:05:02 -0400

    add spinn class

--- src/containers/jp/class-signup/types/advanced/index.tsx
+++ src/containers/jp/class-signup/types/advanced/index.tsx
@@ -5,12 +5,14 @@ import advLaser from "./advanced-laser-clinic";
 import keelboat from "./keelboat";
 import laser from "./laser";
 import advMercClinic from "./advanced-merc-clinic";
+import spinnaker from "./spinnaker"
 
 export default [
 	advMercClinic,
 	laser,
 	ct_420,
 	keelboat,
+	spinnaker,
 	advLaser,
 	adv420,
 	advKeelboat
