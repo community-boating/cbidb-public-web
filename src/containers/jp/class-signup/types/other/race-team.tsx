95266f3e9d89253c8cf9d4e256ce62243273c313
evan-mccarty <evan.t.mccarty@gmail.com>
2023-11-27 11:20:00 -0500

    Update race-team.tsx

--- src/containers/jp/class-signup/types/other/race-team.tsx
+++ src/containers/jp/class-signup/types/other/race-team.tsx
@@ -4,12 +4,12 @@ import { ClassType } from "../../class-description";
 const data: ClassType = {
 	typeId: 281,
 	typeName: "Race Team",
-	prereq: "Laser and 420 Ratings",
+	prereq: "Race Team Rating",
 	sessionCt: 5,
 	sessionLength: 3,
 	classSize: 0,
 	description: (<React.Fragment>
-		Racing skills are honed through practices and CBI weekly racing series. The team competes in regattas at sailing centers throughout the area.
+		Racing skills are honed through practices and CBI weekly racing series. The team competes in regattas at sailing centers throughout the area. In order to join the Race Team, Juniors must earn the racing rating from the Intro to Racing class.
 		Please see the <a href="https://www.community-boating.org/programs/junior-program/race-team/" target ="_blank">Race Team page</a> for more information.
 		If your junior sailor is new to CBI but has prior racing experience and would like to join the race team,
 		please contact the Junior Program Director at <a href="mailto:juniorprogramdirector@community-boating.org">juniorprogramdirector@community-boating.org</a>.
