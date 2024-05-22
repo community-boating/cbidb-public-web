51eef418cf7fd1313be047a05704ffda832bb6a3
alexjpbanks14 <alexjpbanks14@gmail.com>
2024-05-22 03:53:47 -0400

    fix for cycler

--- src/async/providers/APClassInstancesProvider.tsx
+++ src/async/providers/APClassInstancesProvider.tsx
@@ -11,20 +11,30 @@ const defaultAPClassInstances: t.TypeOf<typeof validator> = [];
 
 const currentDate = moment().format('mm/dd/YYYY');
 
+const fakeTimeA = '01:00 AM'
+const fakeTimeB = '01:01 AM'
+
 const fakeAPClassInstances: typeof defaultAPClassInstances = [
     {
         instanceId: 1,
         typeName: option.some('A Class'),
         startDate: currentDate,
-        startTime: '09:45 PM',
+        startTime: fakeTimeA,
         locationString: option.some('Some Place Over Here'),
         enrollees: 0
-
+    },
+    {
+        instanceId: 2,
+        typeName: option.some('A Class'),
+        startDate: currentDate,
+        startTime: fakeTimeB,
+        locationString: option.some('Some Place Over Here'),
+        enrollees: 0
     }
 ]
 
 export const APClassInstancesContext = React.createContext(defaultAPClassInstances);
 
 export default function APClassInstancesProvider(props: {children?: React.ReactNode}){
-    return <AsyncStateProvider apiWrapper={getAPClassInstances} initState={defaultAPClassInstances} refreshRate={30*1000} makeChildren={(state) => {return <APClassInstancesContext.Provider value={state}>{props.children}</APClassInstancesContext.Provider>}}/>
+    return <AsyncStateProvider apiWrapper={getAPClassInstances} initState={defaultAPClassInstances} refreshRate={30*1000} makeChildren={(state) => {return <APClassInstancesContext.Provider value={fakeAPClassInstances}>{props.children}</APClassInstancesContext.Provider>}}/>
 }
